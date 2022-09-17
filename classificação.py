3d038bc4bf413392049b4052834efaed98cb2efb
Sarah-Lorencon <82477978+Lorencon@users.noreply.github.com>
2022-09-16 22:34:40 -0300

    add classification

---
+++ classificação.py
@@ -0,0 +1,17 @@
+import numpy as np
+import time
+from datetime import datetime
+import math
+import matplotlib.pyplot as plt
+plt.rcParams['figure.figsize'] = (14,6)
+from IPython.display import HTML, Audio
+import librosa
+import librosa.display as ld
+import seaborn as sn
+sn.set()
+
+librosa.util.list_examples()
+
+#filename = librosa.util.example_audio_file()
+filename = librosa.ex('Áudio_de_Ramom_Landim(32)')
+mono_data, sample_rate = librosa.load(filename, duration = 13)
